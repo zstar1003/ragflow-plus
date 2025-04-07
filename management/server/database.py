8751af209be93daedcec89fdec0d40939e8749d0
zstar <65890619+zstar1003@users.noreply.github.com>
2025-04-07 11:59:31 +0800

    refactor(database): 将数据库主机配置从容器内部切换为本地，定期更新交流群二维码 (#17)
    将数据库连接配置中的主机从`host.docker.internal`切换为`localhost`，以便在本地开发环境中更方便地访问数据库。

--- management/server/database.py
+++ management/server/database.py
@@ -4,8 +4,8 @@ from datetime import datetime
 
 # 数据库连接配置
 db_config = {
-    "host": "host.docker.internal", # 如果是在Docke容器内部访问数据库
-    # "host": "localhost",
+    # "host": "host.docker.internal", # 如果是在Docke容器内部访问数据库
+    "host": "localhost",
     "port": 5455,
     "user": "root",
     "password": "infini_rag_flow",
